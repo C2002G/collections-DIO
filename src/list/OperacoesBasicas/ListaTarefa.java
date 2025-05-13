773865cf6e18339e50d37f3630c13f5dde74dd68
C2002G <claudiogabriel2002@gmail.com>
2025-05-13 14:18:50 -0300

    foi 2

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -10,4 +10,17 @@ public class ListaTarefa {
     public ListaTarefa() {
         this.tarefaList = new ArrayList<>();
     }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();//criando uma lista de descrições para remover
+        for(Tarefa t: tarefaList){ //atribuiu o t para tarefalist
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+               tarefasParaRemover.add(t);
+            }
+        }
+       tarefaList.removeAll(tarefasParaRemover);
+    }
 }
